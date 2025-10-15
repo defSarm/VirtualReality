5f556cbabb6b1cce7e3fe76c838182b74b4d7416
Samantha Chiu <156865371+defSarm@users.noreply.github.com>
2025-10-15 01:40:18 +0000

    Unit2Practical2_1ANSWERS

--- VR_Unit2/VRPractical2_1/script.js
+++ VR_Unit2/VRPractical2_1/script.js
@@ -10,8 +10,13 @@ window.addEventListener("DOMContentLoaded",function() {
   for(let i = 0; i < 100; i++){
     let x = rnd(-20,20);
     let z = rnd(-20,20);
-    createTree(x,0,z);
+    let r = rnd(0, 180);
+    createTree(x,-0.5,z);
+    createCloud(x, 8, z, r);
+    createHouse(x, z, r);
+    
   }
+  createHouse(0, 0);
    //Task 2: Use the createCloud(...)  to add several clouds to the scene at various positions.
 
    //Task 4: Use the createHouse(...)  to add several houses to the scene at various positions.
@@ -25,6 +30,31 @@ window.addEventListener("DOMContentLoaded",function() {
       5) Set cloud entities position to those passed in to the function.
       6) Add the cloud entity to the scene
 */
+function createCloud(x, y, z, rotate){
+  let cloud = document.createElement("a-entity");
+
+  let sphereL = document.createElement("a-sphere");
+  sphereL.setAttribute("radius", "0.5");
+  sphereL.setAttribute("position", {x:-0.75, y:0, z:0});
+
+  let sphereM = document.createElement("a-sphere");
+  sphereM.setAttribute("radius","0.75");
+
+  let sphereR = document.createElement("a-sphere");
+  sphereR.setAttribute("radius", "0.5");
+  sphereR.setAttribute("position", {x:0.75, y:0, z:0});
+
+  cloud.append(sphereL);
+  cloud.append(sphereM);
+  cloud.append(sphereR);
+
+  cloud.setAttribute("position", {x:x, y:y, z:z});
+  cloud.setAttribute("rotation", {x:0, y:rotate, z:0});
+
+
+  scene.append(cloud);
+
+}
 
 /* Task 3: Create a function createHouse that,
       1) Accept an x and z position for where to place the house "entity"
@@ -35,6 +65,87 @@ window.addEventListener("DOMContentLoaded",function() {
       5) Set house entities position to those passed in to the function.
       6) Add the house entity to the scene
 */
+function createHouse(x, z, rotate){
+  let house = document.createElement("a-entity");
+  let base = document.createElement("a-entity");
+  let roof = document.createElement("a-entity");
+
+  // house base
+  let frontwall = document.createElement("a-box");
+  frontwall.setAttribute("depth", 0.1);
+  frontwall.setAttribute("height", 3);
+  frontwall.setAttribute("width", 5);
+  frontwall.setAttribute("position", {x:0, y:1.5, z:0});
+
+  let leftwall = document.createElement("a-box");
+  leftwall.setAttribute("depth", 0.1);
+  leftwall.setAttribute("height", 3);
+  leftwall.setAttribute("width", 5);
+  leftwall.setAttribute("position", {x:-2.5, y:1.5, z:-2.5});
+  leftwall.setAttribute("rotation", "0 90 0");
+
+  let rightwall = document.createElement("a-box");
+  rightwall.setAttribute("depth", 0.1);
+  rightwall.setAttribute("height", 3);
+  rightwall.setAttribute("width", 5);
+  rightwall.setAttribute("position", {x:2.5, y:1.5, z:-2.5});
+  rightwall.setAttribute("rotation", "0 -90 0");
+
+  let backwall = document.createElement("a-box");
+  backwall.setAttribute("depth", 0.1);
+  backwall.setAttribute("height", 3);
+  backwall.setAttribute("width", 5);
+  backwall.setAttribute("position", {x:0, y:1.5, z:-5});
+
+  base.append(frontwall);
+  base.append(leftwall);
+  base.append(rightwall);
+  base.append(backwall);
+
+  // house roof
+  let frontTri = document.createElement("a-triangle");
+  frontTri.setAttribute("side", "double");
+  frontTri.setAttribute("position", "0 0 2");
+  frontTri.setAttribute("scale", "10 1 1");
+
+  let leftRoof = document.createElement("a-box");
+  leftRoof.setAttribute("rotation", "0 0 -80");
+  leftRoof.setAttribute("width", 0.1);
+  leftRoof.setAttribute("depth", 5);
+  leftRoof.setAttribute("height", 5.1);
+  leftRoof.setAttribute("position", "-2.5 0 0")
+
+  let rightRoof = document.createElement("a-box");
+  rightRoof.setAttribute("rotation", "0 0 80");
+  rightRoof.setAttribute("width", 0.1);
+  rightRoof.setAttribute("depth", 5);
+  rightRoof.setAttribute("height", 5.1);
+  rightRoof.setAttribute("position", "2.5 0 0")
+
+  let backTri = document.createElement("a-triangle");
+  backTri.setAttribute("side", "double");
+  backTri.setAttribute("position", "0 0 -2");
+  backTri.setAttribute("scale", "10 1 1");
+
+  roof.append(frontTri);
+  roof.append(backTri);
+  roof.append(leftRoof);
+  roof.append(rightRoof);
+  roof.setAttribute("position", "0 3.5 -2.5");
+  roof.setAttribute("scale", "0.5 1 1.25");
+
+
+  house.append(base);
+  house.append(roof);
+
+  house.setAttribute("scale", "0.25 0.5 0.5");
+  house.setAttribute("position", {x:x, y:-0.5, z:z});
+  house.setAttribute("rotation", {x:0, y:rotate, z:0});
+
+  scene.append(house);
+
+}
+
 function createTree(x, y, z){
   let tree = document.createElement("a-entity");
   
