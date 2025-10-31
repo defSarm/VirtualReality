8c5a3fe88ff59cf7fbcddd2b1a1170c634b27d23
Samantha Chiu <156865371+defSarm@users.noreply.github.com>
2025-10-31 12:41:23 +0000

    2_7ClassworkAnswers

--- VR_Unit2/VRLesson2_7Classwork/dude.js
+++ VR_Unit2/VRLesson2_7Classwork/dude.js
@@ -4,6 +4,8 @@ class Dude{
     this.y = y;
     this.z = z;
     //Challenge 1: Add delta variables in order to make the dude walk forward on the z axis and jump up and down on the y axis.
+    this.dz = 0.01;
+    this.dy = 0.1;
 
     this.obj = dudeTemplate.cloneNode(true);
     this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
@@ -11,6 +13,13 @@ class Dude{
   }
   jump(){
     //Challenge 2: Add the accumulators to modify the y and z variables
+
+    this.y += this.dy;
+    if (this.y > 5 || this.y < 1){
+      this.dy = -this.dy;
+    }
+
+    this.z+=this.dz;
 
     //Challenge 3: Add the decision in order to alternate the delta on the y axis when the dude reaches less then 1 or greater than 2
 
--- VR_Unit2/VRLesson2_7Classwork/script.js
+++ VR_Unit2/VRLesson2_7Classwork/script.js
@@ -1,16 +1,22 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene, dudeTemplate;
+let dudes = [];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
-  
+  for (let i = 0; i<10; i++){
+    dudes.push (new Dude(rnd(-20, 20), 0.6, rnd(-20,20)));
+  }
+
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
-  
+  for (let dude of dudes){
+    dude.jump();
+  }
   window.requestAnimationFrame( loop );
 }
