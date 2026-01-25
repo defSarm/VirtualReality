cfacb3bd65166685ca8303f10494b4253afd8d7c
Samantha Chiu <156865371+defSarm@users.noreply.github.com>
2026-01-25 21:20:32 +0000

    update2

--- Escaping_Reality/script.js
+++ Escaping_Reality/script.js
@@ -1,4 +1,7 @@
-let scene;
+let scene,handslot;
+let con1=[], con2=[], con3=[], con4=[], con5=[];
+let conveyoritems = [con1,con2,con3,con4,con5];
+let hands = [];
 
 function rnd(l, u){
   return Math.floor(Math.random()*(u-l) + l);
@@ -6,6 +9,9 @@ function rnd(l, u){
 
 window.addEventListener("DOMContentLoaded",function() {
     scene = document.querySelector("a-scene");
+    camera = document.querySelector("a-camera");
+    handslot = document.querySelector("#handslot");
+
 
     topbun = document.querySelector("#topbun");
     botbun = document.querySelector("#botbun");
@@ -16,10 +22,123 @@ window.addEventListener("DOMContentLoaded",function() {
     setTimeout(loop, 1000);
 })
 
+// function checks if the conveyor is full or not and continues to check
+function conveyorcheck(){
+    if (limit1<5 || limit2<5 || limit3<5 || limit4<5 || limit5<5){
+        while (conveyNum == 1){
+            if(limit1<5){
+                limit1++;
+                return true;
+            } else{
+                conveyNum++;
+            }
+        }
+        while (conveyNum == 2){
+            if(limit2<5){
+                limit2++;
+                return true;
+            } else{
+                conveyNum++;
+            }
+        }
+        while (conveyNum == 3){
+            if(limit3<5){
+                limit3++;
+                return true;
+            } else{
+                conveyNum++;
+            }
+        }
+        while (conveyNum == 4){
+            if(limit4<5){
+                limit4++;
+                return true;
+            } else{
+                conveyNum++;
+            }
+        }
+        while (conveyNum == 5){
+            if(limit5<5){
+                limit5++;
+                return true;
+            } else{
+                conveyNum=1;
+            }
+        }
+    }
+    else{
+        return false;
+    }
+}
+
+// function puts all items into an array of arrays
+function list(item){
+    if (conveyNum==1){
+        con1.push(item);
+    }
+    if (conveyNum==2){
+        con2.push(item);
+    }
+    if (conveyNum==3){
+        con3.push(item);
+    }
+    if (conveyNum==4){
+        con4.push(item);
+    }
+    if (conveyNum==5){
+        con5.push(item);
+    }
+}
+
 function loop(){
   // skip by 3 x
-    new Conveyor(1, -8).randomize();
-  
+    item = new Conveyor(-8);
+    for (food of hands){
+        if (food.id == "topbun"){            
+            handslot.append(food);
+            food.setAttribute("position", {x:0,y:2, z:0});
+            
+            console.log(food.object3D.position);
+            
+        }
+    }
+
+    for (food of conveyoritems){
     
+        if (food.length==5){
+            
+            for (items of food){
+                
+
+                if(food.indexOf(items)==food.length-1 && items.object3D.position.z<-0.8){
+                    items.object3D.position.z = -0.8;
+                    //console.log("running");
+                }
+                if(food.indexOf(items)==food.length-2 && items.object3D.position.z<-2.4){
+                    items.object3D.position.z = -2.4;
+                    //console.log("running2");
+                }
+                if(food.indexOf(items)==food.length-3 && items.object3D.position.z<-4){
+                    items.object3D.position.z = -4;
+                    //console.log("running3");
+                }
+                if(food.indexOf(items)==food.length-4 && items.object3D.position.z<-5.4){
+                    items.object3D.position.z = -5.4;
+                    //console.log("running4");
+                }
+                if(food.indexOf(items)==food.length-5 && items.object3D.position.z<-6.8){
+                    items.object3D.position.z = -6.8;
+                    //console.log("running5");
+                }
+            }
+        }
+    }
     window.requestAnimationFrame( loop );
 }
+
+    
+    
+  
+    
+    
+
